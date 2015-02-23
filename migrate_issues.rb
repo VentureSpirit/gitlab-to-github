994970c0fdf6304507832e9fec263293a25b21ea
Lien Van Den Steen <lienvandensteen@gmail.com>
2015-02-23 16:41:36 +0100

    Updated script so it will create labels and issues and comments

--- migrate_issues.rb
+++ migrate_issues.rb
@@ -14,13 +14,22 @@ def github_client(username)
   GITHUB_CLIENTS[username] || GITHUB_CLIENTS.first.last
 end
 
-# gitlab username, payload must include +title+ and +body+, and can optionally include +milestone+
+# gitlab username, payload must include +title+ and +body+, and can optionally include +milestone+ and +labels+
 def create_issue(username, payload)
-  # TODO map milestones
   github_client(username).issues.with(user: CONFIG["github"]["project_user"], repo: CONFIG["github"]["project_repo"]).create(payload)
 end
 
-# TODO: def create_comment()
+def update_issue(username, issue_number, payload)
+  github_client(username).issues.with(user: CONFIG["github"]["project_user"], repo: CONFIG["github"]["project_repo"],  number: issue_number).edit(payload)
+end
+
+def create_comment(username, issue_number, payload)
+  github_client(username).issues.comments.with(user: CONFIG["github"]["project_user"], repo: CONFIG["github"]["project_repo"],  number: issue_number).create(payload)
+end
+
+def create_label(username, payload)
+  github_client(username).issues.labels.with(user: "venturespirit", repo: CONFIG['github']['project_repo']).create(payload)
+end
 
 # For debugging
 # require 'pry'
@@ -30,6 +39,17 @@ issues = []
 comments = {}
 uploads = []
 users = []
+labels = []
+
+# Get all the labels and create the labels on github
+# duplicate label names will return error code 422
+labels = Gitlab.labels(CONFIG["gitlab"]["project_id"])
+labels.each do |label|
+  colorlabel = label.color.delete! '#'
+  username = CONFIG["github"]["label_creator"]
+  create_label(username, {color: colorlabel, name: label.name})
+end
+puts "Got #{labels.count} labels"
 
 # Get all the issues
 page = 0
@@ -53,7 +73,6 @@ users = issues.map { |issue| issue.author.username }
 users += comments.values.flatten.map { |comment| comment.author.username }
 users.uniq!
 puts "Got #{users.count} users"
-
 p users
 
 # Fetch all the uploads
@@ -68,4 +87,34 @@ comments.values.flatten.each do |comment|
 end
 puts "Got #{uploads.count} uploads"
 
-# uploads.each { |u| p u }
+uploads.each { |u| p u }
+
+p "create the issues"
+# create all the issues on github
+issues.each do |issue|
+  payload = {
+    title: issue.title,
+    body: issue.description,
+    labels: issue.labels
+  }
+
+  if issue.milestone
+    payload['milestone'] = CONFIG['milestone_mapping'][issue.milestone.title]
+  end
+
+  if issue.assignee
+    payload['assignee'] = CONFIG['gitlab_user_to_github_user_mapping'][issue.assignee.username]
+  end
+
+  githubissue = create_issue(issue.author.username, payload)
+  if issue.state === "closed"
+    puts "update the issue's state to closed"
+    update_issue(issue.author.username, githubissue.body.number, {state: "closed"})
+  end
+
+  comments[issue.id].each do |comment|
+    puts "adding comments to issue"
+    create_comment(comment.author.username, githubissue.body.number, {body: comment.body})
+  end
+
+end
